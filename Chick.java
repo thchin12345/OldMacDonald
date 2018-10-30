4cbabd223e0b5d9aadb1c5e27afe0f9bfc6e60ad
THOMAS CHIN <THCHIN@sfusd.edu>
2018-10-30 14:32:11 -0700

    firstcommit

--- Chick.java
+++ Chick.java
@@ -1,4 +1,23 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
-     //your code here
+  private String myType;     
+  private String mySound;      
+  public Chick(String type, String sound, String sound2) {         
+    myType = type;         
+    if ((int)(Math.random()*2)==1)
+      mySound = sound;
+    else
+      mySound = sound2;
+   
+  }     
+  public Chick() {         
+    myType = "unknown";         
+    mySound = "unknown";
+  }      
+  public String getSound() {
+    return mySound;
+  }     
+  public String getType() {
+    return myType;
+  }    //your code here
 }
--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,14 @@
-class NamedCow
+class NamedCow extends Cow 
 {
+  private String myName;
 
+  public NamedCow(String type, String sound, String name ) {
+    myType=type;
+    mySound=sound;
+    myName=name;
+  }
+  
+  public String getName() {
+    return myName;
+  }
 }
